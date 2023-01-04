550ddda8e890821b1a9e2ed436246b1d572b446d
taraswww777 <taraswww777@mail.ru>
2023-01-04 13:33:34 +0300

    #2 make integrate with font awesome

--- resources/js/vue-bootstrap.js
+++ resources/js/vue-bootstrap.js
@@ -1,15 +1,24 @@
 import {createApp} from 'vue'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {faBars, faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
+import {faUser, faClock} from '@fortawesome/free-regular-svg-icons'
 import RootComponent from '../vue/RootComponent';
 import * as allComponents from "../vue/components";
 
 const app = createApp(RootComponent);
 
-function registerComponents(components){
+function registerComponents(components) {
     const componentKeys = Object.keys(components);
     componentKeys.forEach((key) => {
         app.component(key, components[key]);
     });
 }
+
+library.add(faBars, faMagnifyingGlass);
+library.add(faUser, faClock);
+
+app.component('Fa', FontAwesomeIcon);
 
 registerComponents(allComponents);
 
