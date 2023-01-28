326cd3d0a15a23d5a9549cbcd133ca1738076663
taraswww777 <taraswww777@mail.ru>
2023-01-28 07:52:38 +0300

    #31 add template input-message-text

--- resources/js/fe-sky-app/components/dialog/DialogPanel.vue
+++ resources/js/fe-sky-app/components/dialog/DialogPanel.vue
@@ -20,7 +20,10 @@
             </div>
         </div>
         <div :class="bem('input-message-text')">
-            ferf
+            <button :class="bem('btn-rec')" type="button">
+                <img src="./mic.svg">
+            </button>
+            <textarea  :class="bem('textarea')"></textarea>
         </div>
     </div>
 </template>
@@ -44,11 +47,14 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import '../../../../sass/mixins';
+
 .dialog-panel {
+    @include borderRadius;
+
     display: flex;
     flex-wrap: nowrap;
     width: 100%;
-    border-radius: 8px;
     max-height: 100vh;
     flex-direction: column;
     overflow: hidden;
@@ -97,6 +103,29 @@ export default {
         width: 100%;
         padding: 25px 20px;
         background: #EAEEF6;
+        display: flex;
+        flex-wrap: nowrap;
+    }
+
+    &__btn-rec {
+        @include borderCircle;
+        padding: 12px 13.5px;
+        background: linear-gradient(45deg, #D485F1, #7156F8);
+        cursor: pointer;
+        width: fit-content;
+        height: fit-content;
+    }
+
+    &__textarea{
+        @include borderRadius;
+        margin: 0 0 0 16px;
+        padding: 10px 18px;
+        color: #737373;
+        min-height: 52px;
+        resize: none;
+        font-size: 13px;
+        line-height: 1.1;
+        flex-grow: 1;
     }
 }
 </style>
