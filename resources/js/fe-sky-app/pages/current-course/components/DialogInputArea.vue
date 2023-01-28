9cd6f7eaf74af7df99b0b701180d7198f6783b23
taraswww777 <taraswww777@mail.ru>
2023-01-28 08:17:30 +0300

    #31 separate DialogInputArea.vue

---
+++ resources/js/fe-sky-app/pages/current-course/components/DialogInputArea.vue
@@ -0,0 +1,55 @@
+<template>
+    <div :class="bem()">
+        <button :class="bem('btn-rec')" type="button">
+            <img src="./mic.svg">
+        </button>
+        <textarea :class="bem('textarea')"></textarea>
+    </div>
+</template>
+
+<script>
+import useBem from "vue3-bem";
+
+const componentName = 'DialogInputArea';
+const bem = useBem(componentName);
+
+export default {
+    name: componentName,
+    data: () => ({
+        bem,
+    }),
+}
+</script>
+
+<style scoped lang="scss">
+@import '../../../../../sass/mixins';
+
+.dialog-input-area {
+    width: 100%;
+    padding: 25px 20px;
+    background: #EAEEF6;
+    display: flex;
+    flex-wrap: nowrap;
+
+    &__btn-rec {
+        @include borderCircle;
+        padding: 12px 13.5px;
+        background: linear-gradient(45deg, #D485F1, #7156F8);
+        cursor: pointer;
+        width: fit-content;
+        height: fit-content;
+    }
+
+    &__textarea {
+        @include borderRadius;
+        margin: 0 0 0 16px;
+        padding: 10px 18px;
+        color: #737373;
+        min-height: 52px;
+        resize: none;
+        font-size: 13px;
+        line-height: 1.1;
+        flex-grow: 1;
+    }
+}
+</style>
--- resources/js/fe-sky-app/pages/current-course/components/DialogPanel.vue
+++ resources/js/fe-sky-app/pages/current-course/components/DialogPanel.vue
@@ -19,11 +19,8 @@
                 />
             </div>
         </div>
-        <div :class="bem('input-message-text')">
-            <button :class="bem('btn-rec')" type="button">
-                <img src="./mic.svg">
-            </button>
-            <textarea  :class="bem('textarea')"></textarea>
+        <div :class="bem('dialog-input-area')">
+            <DialogInputArea/>
         </div>
     </div>
 </template>
@@ -31,6 +28,7 @@
 <script>
 import useBem from "vue3-bem";
 import Message from "./Message.vue";
+import DialogInputArea from "./DialogInputArea.vue";
 
 const componentName = 'DialogPanel';
 const bem = useBem(componentName);
@@ -38,7 +36,8 @@ const bem = useBem(componentName);
 export default {
     name: componentName,
     components: {
-        Message
+        Message,
+        DialogInputArea
     },
     data: () => ({
         bem,
@@ -99,33 +98,8 @@ export default {
         }
     }
 
-    &__input-message-text {
+    &__dialog-input-area {
         width: 100%;
-        padding: 25px 20px;
-        background: #EAEEF6;
-        display: flex;
-        flex-wrap: nowrap;
-    }
-
-    &__btn-rec {
-        @include borderCircle;
-        padding: 12px 13.5px;
-        background: linear-gradient(45deg, #D485F1, #7156F8);
-        cursor: pointer;
-        width: fit-content;
-        height: fit-content;
-    }
-
-    &__textarea{
-        @include borderRadius;
-        margin: 0 0 0 16px;
-        padding: 10px 18px;
-        color: #737373;
-        min-height: 52px;
-        resize: none;
-        font-size: 13px;
-        line-height: 1.1;
-        flex-grow: 1;
     }
 }
 </style>
