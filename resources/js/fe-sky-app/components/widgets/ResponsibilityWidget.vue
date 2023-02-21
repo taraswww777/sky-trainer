3e6c379500fde89828bd7bc2f9a15b65aaa1c683
taraswww777 <taraswww777@mail.ru>
2023-02-21 08:46:44 +0300

    #72 add tmpl GrammarErrorsWidget              MistakesNegotiationsWidget              PrintSpeedWidget              ProgressWidget              ResponsibilityWidget              ScriptMatchWidget              UserCardWidget              VoiceAnalysisWidget

---
+++ resources/js/fe-sky-app/components/widgets/ResponsibilityWidget.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :class="bem()">
+    <h3 :class="bem('title')">
+      Ответственность
+    </h3>
+  </div>
+</template>
+<script>
+import useBem from 'vue3-bem';
+
+const name = 'ResponsibilityWidget';
+const bem = useBem(name);
+
+export default {
+  name,
+  data: () => ({
+    bem,
+  }),
+};
+</script>
+<style lang="scss" scoped>
+@import "../../../../sass/media";
+
+.ResponsibilityWidget {
+
+}
+</style>
---
+++ resources/js/fe-sky-app/components/widgets/VoiceAnalysisWidget.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :class="bem()">
+    <h3 :class="bem('title')">
+      Анализ голоса
+    </h3>
+  </div>
+</template>
+<script>
+import useBem from 'vue3-bem';
+
+const name = 'VoiceAnalysisWidget';
+const bem = useBem(name);
+
+export default {
+  name,
+  data: () => ({
+    bem,
+  }),
+};
+</script>
+<style lang="scss" scoped>
+@import "../../../../sass/media";
+
+.VoiceAnalysisWidget {
+
+}
+</style>
