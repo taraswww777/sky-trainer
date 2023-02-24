89f63299ace5e128879e6c16c627ef259d1e53f5
taraswww777 <taraswww777@mail.ru>
2023-02-24 22:57:51 +0300

    #68 add root styles

--- resources/js/fe-sky-app/RootComponent.vue
+++ resources/js/fe-sky-app/RootComponent.vue
@@ -1,13 +1,50 @@
 <template>
-  <router-view></router-view>
+  <div :class="bem()">
+    <div :class="bem('loader')">
+      <Loader/>
+    </div>
+    <div :class="bem('page')">
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
 
 <script>
+import useBem from 'vue3-bem';
 import {refreshCurrentUser} from './app-store/auth';
+import {Loader} from './components';
+
+const name = 'Root';
+const bem = useBem(name);
 
 export default {
+  name,
+  components: {
+    Loader,
+  },
+  data: () => ({
+    bem,
+  }),
   mounted() {
     refreshCurrentUser(this.$store.dispatch);
   },
 };
 </script>
+<style lang="scss" scoped>
+.root {
+  position: relative;
+  min-height: 100vh;
+  width: 100%;
+  overflow-x: hidden;
+
+  &__loader {
+    position: absolute;
+  }
+
+  &__page {
+    position: relative;
+    width: 100%;
+    min-height: 100vh;
+  }
+}
+</style>
