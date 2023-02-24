fc315d5da546d73fc37bedfa67723a8b686ccda7
taraswww777 <taraswww777@mail.ru>
2023-02-24 23:25:13 +0300

    #68 fix template PageLogin

--- resources/js/fe-sky-app/pages/LoginPage.vue
+++ resources/js/fe-sky-app/pages/LoginPage.vue
@@ -1,230 +1,242 @@
 <template>
-    <div :class="bem()">
-		<div :class="bem('logo')">
-			<router-link to="/">
-				<img src="../../../img/logo_white.svg" alt="">
-			</router-link>
-		</div>
-
-		<div :class="bem('cols')">
-			<div :class="bem('coll')">
-				<div :class="bem('title')">С каждым днём ты становишься лучше, чем вчера!</div>
-
-				<div :class="bem('slider')">
-					<div :class="bem('slide')">
-						<div :class="bem('desc')">Приветствуем вас в logoapp тут вы станете умнее и&nbsp;научитесь  новому. Просто запускайте курсы и тренируйтесь!</div>
-					</div>
-				</div>
-			</div>
-
-			<div :class="bem('colr')">
-				<LoginForm/>
-			</div>
-        </div>
+  <div :class="bem()">
+    <div :class="bem('logo')">
+      <router-link to="/">
+        <img src="../../../img/logo_white.svg" alt="">
+      </router-link>
     </div>
+
+    <div :class="bem('row')">
+      <div :class="bem('col-left')">
+        <div :class="bem('title')">С каждым днём ты становишься лучше, чем вчера!</div>
+
+        <div :class="bem('slider')">
+          <div :class="bem('slide')">
+            <div :class="bem('desc')">
+              Приветствуем вас в logoapp тут вы станете умнее и&nbsp;научитесь новому. Просто
+              запускайте курсы и тренируйтесь!
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div :class="bem('col-right')">
+        <LoginForm/>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import useBem from "vue3-bem";
+import useBem from 'vue3-bem';
+import {LoginForm} from '../components';
 
 const componentName = 'PageLogin';
 const bem = useBem(componentName);
 
 export default {
-    name: componentName,
-    data: () => ({
-        bem,
-    })
-}
+  name: componentName,
+  components: {
+    LoginForm,
+  },
+  data: () => ({
+    bem,
+  }),
+};
 </script>
 
 <style lang="scss" scoped>
 @import "../../../sass/media";
 
-.page-login{
-	padding: 18px 24px 40px;
-	min-height: 100%;
-	background: linear-gradient(255.78deg, #816AF8 13.11%, #D585F2 182.33%);
-	position: relative;
+.page-login {
+  padding: 18px 24px 40px;
+  min-height: 100vh;
+  width: 100%;
+  background: linear-gradient(255.78deg, #816AF8 13.11%, #D585F2 182.33%);
+  position: relative;
 
-	&:before {
-		content: "";
-		position: absolute;
-		right: 42px;
-		top: -14px;
-		width: 497px;
-		height: 309px;
-		background: url(../../../img/pattern4.svg) 0 0/contain no-repeat;
-	}
+  &:before {
+    content: "";
+    position: absolute;
+    right: 42px;
+    top: -14px;
+    width: 497px;
+    height: 309px;
+    background: url(../../../img/pattern4.svg) 0 0/contain no-repeat;
+  }
 
-	&__logo{
-		display: block;
-		position: relative;
-		z-index: 3;
+  &__logo {
+    display: block;
+    position: relative;
+    z-index: 3;
 
-		a{
-			display: inline-block;
-			vertical-align: top;
-		}
+    a {
+      display: inline-block;
+      vertical-align: top;
+    }
 
-		img{
-			display: block;
-			width: 138px;
-		}
-	}
+    img {
+      display: block;
+      width: 138px;
+    }
+  }
 
-	&__cols{
-		margin-top: 39px;
-		position: relative;
-		z-index: 2;
-	}
+  &__row {
+    margin-top: 39px;
+    position: relative;
+    z-index: 2;
+  }
 
-	&__title{
-		font-weight: 300;
-		font-size: 24px;
-		line-height: 1.25;
+  &__col-left {
 
-		color: #FFFFFF;
-	}
+  }
 
-	&__slider{
-		margin-top: 15px;
-	}
+  &__col-right {
+    margin-top: 46px;
+  }
 
-	&__desc{
-		font-size: 12px;
-		line-height: 1.33;
+  &__title {
+    font-weight: 300;
+    font-size: 24px;
+    line-height: 1.25;
 
-		color: #FFFFFF;
-	}
+    color: #FFFFFF;
+  }
 
-	&__colr{
-		margin-top: 46px;
-	}
+  &__slider {
+    margin-top: 15px;
+  }
 
-	@media (min-width: $mb_small) {
-		&:before {
-			left: 0;
-			right: auto;
-			max-width: 100%;
-		}
+  &__desc {
+    font-size: 12px;
+    line-height: 1.33;
 
-		&__title{
-			font-size: 32px;
-		}
+    color: #FFFFFF;
+  }
 
-		&__slider{
-			margin-top: 20px;
-		}
+  @media (min-width: $mb_small) {
+    &:before {
+      left: 0;
+      right: auto;
+      max-width: 100%;
+    }
 
-		&__desc{
-			font-size: 14px;
-			line-height: 1.71;
-		}
-	}
+    &__title {
+      font-size: 32px;
+    }
 
-	@media (min-width: $mb_middle) {
-		display: flex;
-		justify-content: flex-start;
-		align-items: stretch;
-		align-content: stretch;
-		flex-wrap: wrap;
+    &__slider {
+      margin-top: 20px;
+    }
 
-		padding: 110px 0;
+    &__desc {
+      font-size: 14px;
+      line-height: 1.71;
+    }
+  }
 
-		&:before {
-			right: 49%;
-			width: 51%;
-		}
+  @media (min-width: $mb_middle) {
+    display: flex;
+    justify-content: flex-start;
+    align-items: stretch;
+    align-content: stretch;
+    flex-wrap: wrap;
 
-		&::after{
-			content: '';
-			position: absolute;
-			right: 0;
-			top: 0;
-			width: 49%;
-			height: 100%;
-			background: #fff;
-		}
+    padding: 110px 0;
 
-		&__logo{
-			position: absolute;
-			left: 30px;
-			top: 30px;
+    &:before {
+      right: 49%;
+      width: 51%;
+    }
 
-			img{
-				width: auto;
-			}
-		}
+    &::after {
+      content: '';
+      position: absolute;
+      right: 0;
+      top: 0;
+      width: 49%;
+      height: 100%;
+      background: #fff;
+    }
 
-		&__cols{
-			margin: auto;
-			padding: 0 30px;
-			width: 100%;
-			max-width: 1210px;
+    &__logo {
+      position: absolute;
+      left: 30px;
+      top: 30px;
 
-			display: flex;
-			justify-content: space-between;
-			align-items: flex-start;
-			align-content: flex-start;
-			flex-wrap: wrap;
-		}
+      img {
+        width: auto;
+      }
+    }
 
-		&__coll{
-			width: 46%;
-		}
+    &__row {
+      margin: auto;
+      padding: 0 30px;
+      width: 100%;
+      max-width: 1210px;
 
-		&__title{
-			font-size: 24px;
-		}
+      display: flex;
+      justify-content: space-between;
+      align-items: flex-start;
+      align-content: flex-start;
+      flex-wrap: wrap;
+    }
 
-		&__desc{
-			font-size: 12px;
-			line-height: 1.33;
-		}
+    &__col-left {
+      width: 46%;
+    }
 
-		&__colr{
-			width: 327px;
-			margin-top: 0;
-		}
-	}
+    &__title {
+      font-size: 24px;
+    }
 
-	@media (min-width: $mb_large) {
-		&:before {
-			right: auto;
-			left: -303px;
-			top: -205px;
-			width: 100%;
-			height: 760px;
-		}
+    &__desc {
+      font-size: 12px;
+      line-height: 1.33;
+    }
 
-		&::after {
-			width: 43.75%;
-		}
+    &__col-right {
+      width: 327px;
+      margin-top: 0;
+    }
+  }
 
-		&__coll{
-			width: 47.74%;
-		}
+  @media (min-width: $mb_large) {
+    &:before {
+      right: auto;
+      left: -303px;
+      top: -205px;
+      width: 100%;
+      height: 760px;
+    }
 
-		&__title{
-			font-size: 32px;
-		}
+    &::after {
+      width: 43.75%;
+    }
 
-		&__slider{
-			margin-top: 40px;
-			max-width: 383px;
-		}
+    &__col-left {
+      width: 47.74%;
+    }
 
-		&__desc{
-			font-size: 14px;
-			line-height: 1.71;
-		}
-	}
+    &__title {
+      font-size: 32px;
+    }
 
-	@media (min-width: $mb_exlarge) {
-		&__title{
-			font-size: 48px;
-		}
-	}
+    &__slider {
+      margin-top: 40px;
+      max-width: 383px;
+    }
+
+    &__desc {
+      font-size: 14px;
+      line-height: 1.71;
+    }
+  }
+
+  @media (min-width: $mb_exlarge) {
+    &__title {
+      font-size: 48px;
+    }
+  }
 }
 </style>
