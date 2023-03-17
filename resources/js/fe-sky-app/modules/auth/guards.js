00bdfd43a77a8710d5537f164a916ff64e07f904
taraswww777 <taraswww777@mail.ru>
2023-03-17 13:00:56 +0300

    #98 fir router errors

---
+++ resources/js/fe-sky-app/modules/auth/guards.js
@@ -0,0 +1,11 @@
+import {PAGE_NAMES} from '@srs/constants';
+import {isLogin} from '../app-store/auth';
+
+export const checkAuth = (to, from, next) => {
+  // TODO: Потенциальное место для улучшений
+  //       https://router.vuejs.org/guide/advanced/navigation-guards.html#global-before-guards
+  if (!isLogin() && to.name !== PAGE_NAMES.login && to.name !== PAGE_NAMES.logout) {
+    next({name: PAGE_NAMES.login});
+  }
+  next();
+};
