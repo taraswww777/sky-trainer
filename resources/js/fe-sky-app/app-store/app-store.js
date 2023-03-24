2a39a7ede11bf3a1aa64898c766453ba08a7053f
taraswww777 <taraswww777@mail.ru>
2023-03-24 13:12:13 +0300

    #132 upgrade BasePage

--- resources/js/fe-sky-app/app-store/app-store.js
+++ resources/js/fe-sky-app/app-store/app-store.js
@@ -3,12 +3,14 @@ import {moduleUser} from './module-user';
 import {moduleCourses} from './module-courses';
 import {moduleDialog} from './module-dialog';
 import {moduleLoadings} from './module-loadings';
+import {storePageInfo} from './storePageInfo';
 
 export const appStore = createStore({
   modules: {
     moduleUser,
     moduleCourses,
     moduleDialog,
-    moduleLoadings
+    moduleLoadings,
+    storePageInfo
   }
 });
